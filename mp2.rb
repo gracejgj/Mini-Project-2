c17609debe4841570898a204f4e1dce3ecd5eb80
Jgrace <39512456+Gracej06@users.noreply.github.com>
2020-11-04 17:04:01 +0800

    commit

--- mp2.rb
+++ mp2.rb
@@ -1,72 +1,86 @@
 require 'csv'
 
 class Company
-  attr_accessor :name, :dept, :time
+  attr_accessor :name, :dept
 
-  def initialize(name, dept, time)
+  def initialize(name, dept)
     @name =  name
     @dept = dept
-    @time = time
   end
 
   def save_to_csv
     CSV.open('C:/mp2/log2.csv', 'a+') do |row|
-      row << attrs
+	  row << attrs
     end
   end
 
   def self.read_from_csv
-    CSV.foreach('C:/mp2/log2.csv') do |row|
+    CSV.foreach('log2.csv').uniq do |row|
       puts row.inspect
     end
   end
 
   def attrs
-    [name, dept, time]
+    [name, dept]
+  end
+
+end
+
+class Employee
+  class << self
+  def show_menu
+    puts "Welcome to Company Directory Management System: "
+    puts "1 - Add new employee"
+    puts "2 - Read existing employee"
+    puts "3 - Update existing employee"
+    puts "4 - Delete existing employee"
+
+    input = gets.chomp
+
+    case input
+      when '1'
+        add
+      when '2'
+        read
+      when '3'
+        update
+      when '4'
+        delete
+    end
+  end
+
+  def add
+		puts "Type in your name here:"
+		name = gets.chomp
+		puts "Type in your department here:"
+		dept = gets.chomp
+
+		emp = Company.new(name, dept)
+		emp.save_to_csv
+		puts "Employee record has been added. #{name} from #{dept} department."
+  end
+
+  def read
+		puts "Display logs? (y/n?)"
+    log = gets.chomp
+    log == 'y' ? puts( Company.read_from_csv ) : puts( "no logs to display" )
+  end
+
+  def update
+    puts "Enter name to update: "
+    name = gets.chomp.to_s
+    n = @name[name]
+
+      if !n
+        puts "no name found"
+      else
+        puts "Enter the updated data below."
+        puts "Enter "
+
+
+      end
+    end
   end
 end
 
-  puts "Welcome to Company Directory Management System:
-  1. Add new employee
-  2. Read existing employee
-  3. Update existing employee
-  4. Delete existing employee"
-
-#2.List all the current employee
-#3.Without duplicate the existing employee)
-
-  choice = gets.chomp
-  case choice
-    when '1'
-      puts "Type in your name here:"
-      name = gets.chomp
-      puts "Type in your department here:"
-      dept = gets.chomp
-      #else
-      #  puts "That name already exist!
-        emp = Company.new(name, dept, Time.now)
-        emp.save_to_csv
-        puts "Employee record has been added. #{name} from #{dept} department."
-
-    when '2'
-      Company.read_from_csv
-    else
-      puts "No logs to display"
-
-    when '3'
-
-    when '4'
-
-  end
-    
-
-=begin
-Mini Porject(2) Requirement:
-Create a ‘Company Directory Management System’
-User can ADD new employee
-User can READ existing employee(List all the current employee)
-User can UPDATE existing employee(Without duplicate the existing employee)
-User can DELETE existing employee
-Save database in csv format
-code push to github on regular basis
-=end
+Employee.show_menu
